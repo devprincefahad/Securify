bc0f67859367fdef3fa7752ae3a25bdab83d24f6
devprincefahad <1pfchouhan@gmail.com>
2023-10-09 16:55:14 +0530

    BackHandler fix SettingsScreen

--- app/src/main/java/dev/prince/securify/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/prince/securify/ui/settings/SettingsScreen.kt
@@ -1,10 +1,13 @@
 package dev.prince.securify.ui.settings
 
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -13,6 +16,9 @@ import com.ramcosta.composedestinations.annotation.Destination
 fun SettingsScreen(
 
 ){
+
+    val context = LocalContext.current
+
     Column {
         Text(
             modifier = Modifier.align(
@@ -21,5 +27,8 @@ fun SettingsScreen(
             text = "Settings screen",
             fontSize = 26.sp
         )
+    }
+    BackHandler {
+        (context as ComponentActivity).finish()
     }
 }

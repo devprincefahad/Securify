314ce871d07f8c63737835f2e7b5734aa1671bba
devprincefahad <1pfchouhan@gmail.com>
2023-10-10 02:50:05 +0530

    fix password screen nav issue

--- app/src/main/java/dev/prince/securify/ui/passwords/PasswordsScreen.kt
+++ app/src/main/java/dev/prince/securify/ui/passwords/PasswordsScreen.kt
@@ -3,6 +3,9 @@ package dev.prince.securify.ui.passwords
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,7 +16,9 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import dev.prince.securify.ui.destinations.AddPasswordScreenDestination
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun PasswordsScreen(
@@ -23,15 +28,33 @@ fun PasswordsScreen(
 
     val context = LocalContext.current
     val accounts = viewModel.accounts.collectAsState(emptyList())
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "My Passwords"
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {
+                    navigator.navigate(AddPasswordScreenDestination)
+                },
+                icon = { Icon(Icons.Filled.Add, contentDescription = null) },
+                text = { Text(text = "Add New Password") },
+            )
 
-    Column {
-        Text(
-            modifier = Modifier.align(
-                Alignment.CenterHorizontally
-            ),
-            text = "Password screen",
-            fontSize = 26.sp
-        )
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
     }
     BackHandler {
         (context as ComponentActivity).finish()
--- app/src/main/java/dev/prince/securify/util/Util.kt
+++ app/src/main/java/dev/prince/securify/util/Util.kt
@@ -1,14 +1,10 @@
 package dev.prince.securify.util
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import dev.prince.securify.ui.NavGraphs
-import dev.prince.securify.ui.appCurrentDestinationAsState
+import dev.prince.securify.ui.destinations.AddPasswordScreenDestination
 import dev.prince.securify.ui.destinations.Destination
 import dev.prince.securify.ui.destinations.IntroScreenDestination
 import dev.prince.securify.ui.destinations.SetupKeyScreenDestination
 import dev.prince.securify.ui.destinations.UnlockScreenDestination
-import dev.prince.securify.ui.startAppDestination
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
@@ -22,6 +18,7 @@ fun Destination.shouldShowBottomBar(): Boolean {
     return (this !in listOf(
         IntroScreenDestination,
         UnlockScreenDestination,
-        SetupKeyScreenDestination
+        SetupKeyScreenDestination,
+        AddPasswordScreenDestination
     ))
 }

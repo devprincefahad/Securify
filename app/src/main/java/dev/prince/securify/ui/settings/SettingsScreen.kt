09f4e80fe7040e776421eb1eb30375add4796219
devprincefahad <1pfchouhan@gmail.com>
2023-10-12 14:52:03 +0530

    added about bottom sheet

--- app/src/main/java/dev/prince/securify/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/prince/securify/ui/settings/SettingsScreen.kt
@@ -2,8 +2,12 @@ package dev.prince.securify.ui.settings
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,14 +17,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.LockOpen
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +43,7 @@ import com.ramcosta.composedestinations.annotation.Destination
 import dev.prince.securify.ui.theme.Gray
 import dev.prince.securify.ui.theme.poppinsFamily
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun SettingsScreen(
@@ -38,42 +51,92 @@ fun SettingsScreen(
 ) {
 
     val context = LocalContext.current
-    val items = listOf(
-        "About Securify" to Icons.Outlined.Info,
-        "Share Securify" to Icons.Outlined.Share,
-    )
+    var showSheet by remember { mutableStateOf(false) }
 
-    LazyColumn {
-        items(items) { (text, icon) ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Gray
+    val items = listOf(
+        "Update Master Key" to Icons.Outlined.LockOpen,
+        "Share Securify" to Icons.Outlined.Share,
+        "About Securify" to Icons.Outlined.Info
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            modifier = Modifier.padding(vertical = 28.dp),
+            text = "Settings",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+            fontFamily = poppinsFamily
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 24.dp, topEnd = 24.dp
+                    )
                 ),
-                shape = RoundedCornerShape(20.dp),
+            shape = RoundedCornerShape(
+                bottomStart = 0.dp, bottomEnd = 0.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxSize()
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Icon(
-                        imageVector = icon,
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
-                    Text(
-                        text = text,
-                        fontSize = 18.sp,
-                        color = Color.Black,
-                        fontFamily = poppinsFamily,
-                        fontWeight = FontWeight.Medium
-                    )
+                items(items) { (text, icon) ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Gray
+                        ),
+                        shape = RoundedCornerShape(20.dp),
+                        onClick = {
+                            if (text == "About Securify") {
+                                showSheet = true
+                            }
+                        }
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Icon(
+                                imageVector = icon,
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = text,
+                                fontSize = 18.sp,
+                                color = Color.Black,
+                                fontFamily = poppinsFamily,
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+                    }
                 }
+            }
+        }
+        if (showSheet) {
+            AboutBottomSheet {
+                showSheet = false
             }
         }
     }

95d48602259bd9aa12ccdd27467565b173e5a7f3
devprincefahad <1pfchouhan@gmail.com>
2023-10-13 15:06:48 +0530

    display new account details from db

--- app/src/main/java/dev/prince/securify/ui/passwords/PasswordsScreen.kt
+++ app/src/main/java/dev/prince/securify/ui/passwords/PasswordsScreen.kt
@@ -2,8 +2,19 @@ package dev.prince.securify.ui.passwords
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
@@ -11,12 +22,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import dev.prince.securify.database.AccountEntity
 import dev.prince.securify.ui.destinations.AddPasswordScreenDestination
+import dev.prince.securify.ui.theme.poppinsFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
@@ -28,16 +46,8 @@ fun PasswordsScreen(
 
     val context = LocalContext.current
     val accounts = viewModel.accounts.collectAsState(emptyList())
+
     Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "My Passwords"
-                    )
-                }
-            )
-        },
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 onClick = {
@@ -49,15 +59,75 @@ fun PasswordsScreen(
 
         }
     ) { innerPadding ->
-
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(color = Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+            Text(
+                modifier = Modifier.padding(vertical = 28.dp),
+                text = "My Passwords",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = poppinsFamily
+            )
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 24.dp, topEnd = 24.dp
+                        )
+                    ),
+                shape = RoundedCornerShape(
+                    bottomStart = 0.dp, bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                )
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                ) {
+                    items(accounts.value) { account ->
+                        AccountRow(account)
+                    }
+                }
+            }
         }
     }
     BackHandler {
         (context as ComponentActivity).finish()
     }
 
+}
+
+@Composable
+fun AccountRow(account: AccountEntity) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(account.accountName)
+            Text(account.userName)
+            Text(account.email)
+            Text(account.mobileNumber)
+            Text(account.password)
+        }
+    }
 }

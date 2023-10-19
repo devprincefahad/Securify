86f1bef70d8ad3ec2404ef2909aa31b97153f69e
devprincefahad <1pfchouhan@gmail.com>
2023-10-20 00:02:11 +0530

    added keystore for more security

--- app/src/main/java/dev/prince/securify/encryption/EncryptionManager.kt
+++ app/src/main/java/dev/prince/securify/encryption/EncryptionManager.kt
@@ -1,34 +1,64 @@
 package dev.prince.securify.encryption
 
+import android.content.Context
 import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import androidx.annotation.RequiresApi
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.prince.securify.local.SharedPrefHelper
+import java.security.KeyStore
 import java.security.MessageDigest
 import java.util.Base64
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
 import javax.crypto.Mac
+import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import javax.inject.Inject
 
-class EncryptionManager @Inject constructor(
-    prefs: SharedPrefHelper
-) {
+class EncryptionManager @Inject constructor() {
 
-    private val ALGORITHM = "AES"
-    private val TRANSFORMATION = "AES/CBC/PKCS5Padding"
-//    private val SECRET_KEY = generateRandomString()
-    private val SECRET_KEY = "uz_securify_password_key_2023/10"
-//    private val IV = deriveIVFromMasterKey(prefs.masterKey)
+    private val ALIAS = "secure_key_alias"
     private val IV = "=a1Ab2Bc3Cd4De5E".toByteArray(Charsets.UTF_8)
+    private val KEY_STORE = "AndroidKeyStore"
+
+    init {
+        createKeyIfNeeded()
+    }
+
+    private fun createKeyIfNeeded() {
+        val keyStore = KeyStore.getInstance(KEY_STORE)
+        keyStore.load(null)
+
+        if (!keyStore.containsAlias(ALIAS)) {
+            val keyGenerator = KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES, KEY_STORE
+            )
+
+            val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+                ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .setRandomizedEncryptionRequired(false) // You can set this to true if required
+                .build()
+
+            keyGenerator.init(keyGenParameterSpec)
+            keyGenerator.generateKey()
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun encrypt(textToEncrypt: String): String {
-        val secretKeySpec = SecretKeySpec(SECRET_KEY.toByteArray(), ALGORITHM)
-
-        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+        val secretKey = keyStore.getKey(ALIAS, null) as SecretKey
         val ivSpec = IvParameterSpec(IV)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivSpec)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
 
         val encryptedBytes = cipher.doFinal(textToEncrypt.toByteArray(Charsets.UTF_8))
         return Base64.getEncoder().encodeToString(encryptedBytes)
@@ -36,44 +66,17 @@ class EncryptionManager @Inject constructor(
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun decrypt(encryptedText: String): String {
-        val secretKeySpec = SecretKeySpec(SECRET_KEY.toByteArray(), ALGORITHM)
+        val cipher = Cipher.getInstance("${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}")
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+        val secretKey = keyStore.getKey(ALIAS, null) as SecretKey
 
-        val cipher = Cipher.getInstance(TRANSFORMATION)
         val ivSpec = IvParameterSpec(IV)
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivSpec)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec)
 
         val encryptedBytes = Base64.getDecoder().decode(encryptedText)
         val decryptedBytes = cipher.doFinal(encryptedBytes)
         return String(decryptedBytes, Charsets.UTF_8)
-    }
-
-    private fun deriveIVFromMasterKey(masterKey: String): ByteArray {
-        val salt = "securify"
-        val info = "IVGeneration"
-
-        val hmacKey = SecretKeySpec(masterKey.toByteArray(), "HmacSHA256")
-        val hmac = Mac.getInstance("HmacSHA256")
-        hmac.init(hmacKey)
-        val prk = hmac.doFinal(salt.toByteArray())
-
-        val derivedKey = ByteArray(16)
-        val label = "$prk$info".toByteArray()
-        val hash = MessageDigest.getInstance("SHA-256")
-        var currentOffset = 0
-
-        while (currentOffset < 16) {
-            hash.update(label)
-            label.copyInto(derivedKey, currentOffset, 0, minOf(label.size, 16 - currentOffset))
-            currentOffset += label.size
-        }
-        return derivedKey
-    }
-
-    private fun generateRandomString(): String {
-        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..32)
-            .map { allowedChars.random() }
-            .joinToString("")
     }
 
 }
